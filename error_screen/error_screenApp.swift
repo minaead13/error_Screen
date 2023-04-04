4d4bc137e0422424deb4de94e52f2c8a318fe234
mina eid <minaeid6@gmail.com>
2023-04-04 20:20:00 +0200

    Initial Commit

---
+++ error_screen/error_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  error_screenApp.swift
+//  error_screen
+//
+//  Created by Mina on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct error_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
