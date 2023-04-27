9415e0d00b53751d8299d3b812e5e56771675290
mina eid <minaeid6@gmail.com>
2023-04-28 02:15:12 +0300

    Add

--- error_screen/ContentView.swift
+++ error_screen/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
+            Image(uiImage: UIImage(named: "1")!)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .leading, spacing: 30){
+                Text("Oops!...")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                Text("Something wrong with your \nconnection, please try \nagain")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .opacity(0.7)
+                Button(action: {
+                    
+                }) {
+                    Text("Retry".uppercased())
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                        .padding(.vertical)
+                        .padding(.horizontal, 30)
+                        .background(Capsule().foregroundColor(.white))
+                }
+            }
+            .padding(.horizontal,70)
+            .padding(.bottom, UIScreen.main.bounds.height * 0.1) // 10% of our total weight
         }
-        .padding()
     }
 }
 
